890889bb8d3543c24af1344658c559be67caaab0
ChristianLance12 <lancecn@mail.uc.edu>
2021-09-28 16:49:55 -0400

    basically done movement
    hell yea

--- unity/Assets/PlayerMove.cs
+++ unity/Assets/PlayerMove.cs
@@ -5,18 +5,23 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public Transform head;
-    public Transform player;
+    private Transform player;
+    private Vector3 newRot;
     public float speed;
-    public float rotSpeed;
+    public float rotY;
+    public float rotX;
+    public float sprintMultiplier;
     private float currentSpeed;
     private float sprintSpeed;
     private Rigidbody rb;
     void Start()
     {
+        
         rb = this.GetComponent<Rigidbody>();
         player = this.transform;
         currentSpeed = speed;
-        sprintSpeed = speed * 2;
+        sprintSpeed = speed * sprintMultiplier;
+        head.localRotation = Quaternion.Euler(0, 0, 0);
 
 
     }
@@ -26,23 +31,33 @@ public class PlayerMove : MonoBehaviour
     {
         Vector3 rotInput = new Vector3(0, Input.GetAxis("Mouse X"), 0);
         Vector3 headInput = new Vector3(-Input.GetAxis("Mouse Y"), 0, 0);
-        head.Rotate(headInput * Time.deltaTime * rotSpeed);
-        player.Rotate(rotInput * Time.deltaTime * rotSpeed);
-        if (Input.GetKey(KeyCode.W))
+        head.Rotate(headInput * Time.deltaTime * rotY);
+        player.Rotate(rotInput * Time.deltaTime * rotX);
+        var v3 = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        transform.Translate(currentSpeed * v3.normalized * Time.deltaTime);
+        if (head.localRotation.x > 0.6)
         {
-            rb.MovePosition(transform.position + transform.forward * Time.deltaTime * currentSpeed);
+            head.localRotation = Quaternion.Euler(75f, 0, 0);    
+        }
+        else if (head.localRotation.x < -0.6)
+        {
+            head.localRotation = Quaternion.Euler(-75f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {   
+            v3 += Vector3.forward;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            rb.MovePosition(transform.position - transform.forward * Time.deltaTime * currentSpeed);
+            v3 += Vector3.back;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            rb.MovePosition(transform.position - transform.right * Time.deltaTime * currentSpeed);
+            v3 += Vector3.left;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            rb.MovePosition(transform.position + transform.right * Time.deltaTime * currentSpeed);
+            v3 += Vector3.right;
         }
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
@@ -52,8 +67,6 @@ public class PlayerMove : MonoBehaviour
         {
             currentSpeed = speed;
         }
-           
-            
-        
+       
     }
 }
