26cf13b5afd14c935efc8946347903aa048a84db
Joe Boylson <jboylson@saec-kv.com>
2021-10-17 13:07:02 -0400

    wrap get_scenes in try-catch;

--- utils/scene.py
+++ utils/scene.py
@@ -3,42 +3,48 @@ import os
 
 def get_scenes():
 
-    scenes = []
-    for root, _, files in os.walk('./static_content'):
-        for filename in files:
-            if filename == "GUID.txt":
+    try:
 
-                name = root.split("/")[-1]
-                filepath = os.path.join(root, filename)
+        scenes = []
+        for root, _, files in os.walk('static_content'):
+            for filename in files:
+                if filename == "GUID.txt":
 
-                with open(filepath) as guid_txt:
-                    guid = guid_txt.readline().rstrip()
+                    name = root.split("/")[-1]
+                    filepath = os.path.join(root, filename)
 
-                build_folderpath = os.path.join(root, "Build")
-                build_folder_files = os.listdir(build_folderpath)
+                    with open(filepath) as guid_txt:
+                        guid = guid_txt.readline().rstrip()
 
-                loader_url = next(
-                    name for name in build_folder_files if name.endswith(".loader.js"))
-                data_url = next(
-                    name for name in build_folder_files if name.endswith(".data"))
-                framework_url = next(
-                    name for name in build_folder_files if name.endswith(".framework.js"))
-                code_url = next(
-                    name for name in build_folder_files if name.endswith(".wasm"))
+                    build_folderpath = os.path.join(root, "Build")
+                    build_folder_files = os.listdir(build_folderpath)
 
-                # camelCase for JS
-                context = {
-                    "loaderUrl": os.path.join(build_folderpath, loader_url),
-                    "dataUrl": os.path.join(build_folderpath, data_url),
-                    "frameworkUrl": os.path.join(build_folderpath, framework_url),
-                    "codeUrl": os.path.join(build_folderpath, code_url),
-                }
+                    loader_url = next(
+                        name for name in build_folder_files if name.endswith(".loader.js"))
+                    data_url = next(
+                        name for name in build_folder_files if name.endswith(".data"))
+                    framework_url = next(
+                        name for name in build_folder_files if name.endswith(".framework.js"))
+                    code_url = next(
+                        name for name in build_folder_files if name.endswith(".wasm"))
 
-                scenes.append({
-                    "id": guid,
-                    "folderpath": root,
-                    "name": name,
-                    "context": context
-                })
+                    # camelCase for JS
+                    context = {
+                        "loaderUrl": os.path.join(build_folderpath, loader_url),
+                        "dataUrl": os.path.join(build_folderpath, data_url),
+                        "frameworkUrl": os.path.join(build_folderpath, framework_url),
+                        "codeUrl": os.path.join(build_folderpath, code_url),
+                    }
 
-    return scenes
+                    scenes.append({
+                        "id": guid,
+                        "folderpath": root,
+                        "name": name,
+                        "context": context
+                    })
+
+        return scenes
+
+    except Exception as e:
+        print("ERROR GETTING SCENES")
+        print(e)
